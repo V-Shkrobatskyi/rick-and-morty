39549b1ef2f13d6b9579669329bc7ca5eb9d3a4d
Vitalii <vetali5700gmail.com>
2024-07-10 21:40:12 +0300

    add PostgreSQL support

--- rick_and_morty_api/settings.py
+++ rick_and_morty_api/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = "rick_and_morty_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydb",
+        "USER": "myuser",
+        "PASSWORD": "mysecretpassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
